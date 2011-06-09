e79ca60301111e180aee4423e5cc8d7535ac6ebc
t.duponchelle@gmail.com <t.duponchelle@gmail.com@df4151ea-8410-ddf0-4118-7840bdbbc7d7>
2011-06-09 13:04:30 +0000

    Change some compilation scripts (hardcoded)

--- exec.sh
+++ exec.sh
@@ -2,4 +2,4 @@
 
 make
 cd /home/tib/Code/tilem431/trunk/gui
-./tilem2 -r $rom -f /home/tib/Code/z80/oilcap/OILCAP.83p -m /home/tib/Code/z80/oilcap/macro/play.txt
+./tilem2 -r $rom -f /home/tib/Code/z80/oilcapgc/OILCAP.83p -m /home/tib/Code/z80/oilcapgc/macro/play.txt
