171c24f1be71812b09f32aad7d0d7102dcbe3e25
thibaultduponchelle <thibault.duponchelle@gmail.com>
2019-09-13 10:35:53 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 make
-cd /home/tib/Code/tilem530/trunk/gui
-./tilem2 -r $rom -f /home/tib/Code/z80/oilcapgc/OILCAP.83p -m /home/tib/Code/z80/oilcapgc/macro/play.txt
+tilem2 -r $rom -f OILCAP.83p -m ./macro/play.txt
